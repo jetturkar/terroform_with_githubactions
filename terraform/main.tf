551d0c729673c1f9fb7403cf3f83fa73368356dd
jetturkar <135022848+jetturkar@users.noreply.github.com>
2023-06-08 12:28:38 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,17 @@
-terraform {
 
+terraform {
   backend "azurerm" {
-  resource_group_name  = "test-rg"
-  storage_account_name = "testacc26"
-  container_name       = "terraformcontainer"
-  key                  = "terraformcontainer.tfstate"
+    resource_group_name  = "test-rg"
+    storage_account_name = "testacc26"
+    container_name       = "terraformcontainer"
+    key                  = "terraformcontainer.tfstate"
+  }
 }
-  
-  provider "azurerm" {
+
+provider "azurerm" {
   features {}
 }
 
-}
 resource "azurerm_resource_group" "myapplication" {
   name     = "test-rg"
   location = "East US"
